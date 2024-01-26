00982e9110f2fd24a1998588a8413573be57ae99
Ivica Stevanovic <74705280+eeveetza@users.noreply.github.com>
2024-01-26 12:21:37 +0100

    P452 v17.1 (25.04.23) (#6)
    * Update README.md
    
    * Update README.md to include doi
    
    * Create CITATION.cff
    
    * P452 v17.0 (22.05.22)
    
    validation examples in csv instead of xlsx
    
    Update dl_se_ft_inner.m
    
    removed extra semicolons in if command
    
    patch resolving issue #2
    
    - introduced path center latitude (phi_path) as input argument (instead of separate values for phi_t and phi_r)
    - modified validation_examples accordingly
    - use 2.998e8 m/s for speed of light in wavelength calculation (as per ITU-R P.2001) instead of 3e8 m/s
    - updated P452 GUI to account for the input argument changes
    - replaces validation files in .xlsx format with the ones in .csv format
    
    update to validate_p452
    
    - corrected the function call to pl_los with d3D
    - increased the number of digital points in result files
    - check input arguments values
    - update closs_corr.m
    
    Update README and function headers
    
    Update disclaimer in tl_p452.m
    
    Update header in validate_p452.m
    
    Update README.md
    
    * Update README.md
    
    * Update path_fraction.m
    
    - Included correction of the bug in path_fraction (suggested by @mplussier for p1812, see https://github.com/eeveetza/p1812/issues/9)
    
    * patch resolving issue #4
    
    * Updates to pw2p.m resolving Issue #5
    
    * Update README.txt
    
    * P452 v17.1 (25.04.23)

--- matlab/private/path_fraction.m
+++ matlab/private/path_fraction.m
@@ -18,6 +18,7 @@ function   omega = path_fraction(d, zone, zone_r)
 %     Rev   Date        Author                          Description
 %     -------------------------------------------------------------------------------
 %     v0    02FEB16     Ivica Stevanovic, OFCOM         First implementation in matlab
+%     v1    10NOV22     Ivica Stevanovic, OFCOM         Corrected a bug in line 36 (suggested by Martin-Pierre Lussier @mplussier)
 
 dm = 0;
 
@@ -32,7 +33,7 @@ for i = 1:n
     end
     
     if (d(start(i))>0)
-        delta = delta + ( d(stop(i))-d(stop(i)-1) )/2.0;
+        delta = delta + ( d(start(i))-d(start(i)-1) )/2.0;
     end
     
    dm = dm + d(stop(i))-d(start(i)) + delta;
